3daa9e55fd0291414b3f31d3c4b70125a06ef5c9
RubanKumarK <ceeeerubankumar513@gmail.com>
2024-12-28 16:24:00 +0530

    make changes on settings

--- PhotoShopProject/settings.py
+++ PhotoShopProject/settings.py
@@ -116,8 +116,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,  # Timeout in seconds before giving up
+        },
     }
 }
+
 
 
 # Password validation
